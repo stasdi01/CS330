99f05f425afc7ac5d0f4b05c315ed27d113aeb28
stasdi01 <stasdi01@luther.edu>
2024-09-27 16:49:03 -0500

    Submission

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -11,19 +11,40 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    if (clearOnEntry){
+        outputScreen.textContent = "";
+        clearOnEntry = false;
+    }
+    if (outputScreen.textContent === "0" && digit !== "."){
+        outputScreen.textContent = digit;
+    }
+    else{
+        outputScreen.textContent += digit;
+    }
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    clearOnEntry = true;
+    outputScreen.textContent = "0";
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    try{
+        outputScreen.textContent = eval(outputScreen.textContent);
+    }catch(error){
+        outputScreen.textContent = "ERROR"
+    }
+    clearOnEntry = true;
+}
 
 
 /**
@@ -31,10 +52,17 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    if (clearOnEntry){
+        outputScreen.textContent = ""
+        clearOnEntry = false;
+    }
+    outputScreen.textContent += operation
+}
 
 
 window.onload = function () {
     outputScreen = document.querySelector("#result");
     clearOnEntry = true;
+    clear_screen()
 };
