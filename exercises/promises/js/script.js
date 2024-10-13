f4db9f93c103e022dbe48f496cf9f9b14a8a9e04
stasdi01 <stasdi01@luther.edu>
2024-10-12 23:59:07 -0500

    update

--- exercises/promises/js/script.js
+++ exercises/promises/js/script.js
@@ -4,10 +4,30 @@
 'use strict';
 
 async function get_individual(num, all_numbers) {
+    all_numbers.innerHTML = '';
+    try {
+        for (let i = num - 1; i <= num + 1; i++) {
+            let response = await fetch(`http://numbersapi.com/${i}?json`);
+            let data = await response.json();
+            displayTrivia(i, data.text, all_numbers);
+        }
+    } catch (error) {
+        console.error("Error fetching individual number trivia:", error);
+    }
 
 }
 
 async function get_batch(num, all_numbers) {
+    all_numbers.innerHTML = '';
+    try {
+        let response = await fetch(`http://numbersapi.com/${num-1},${num},${num+1}?json`);
+        let data = await response.json();
+        for (let i = num - 1; i <= num + 1; i++) {
+            displayTrivia(i, data[i], all_numbers);
+        }
+    } catch (error) {
+        console.error("Error fetching batch number trivia:", error);
+    }
 
 }
 
